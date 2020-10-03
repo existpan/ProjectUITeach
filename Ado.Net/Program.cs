5a6e67cfc4f8047607a056a03f636f47c3440d95
exist Pan <1468083746@qq.com>
2020-10-03 23:06:46 +0800

    完成了对Insert、Update、Delete方法的编写，返回ExecuteNonQuery方法（通用非查询方法）

--- Ado.Net/Program.cs
+++ Ado.Net/Program.cs
@@ -11,7 +11,9 @@ namespace Ado.Net
     {
         static void Main(string[] args)
         {
-            ConnectDB();
+            //ConnectDB();
+            //Console.WriteLine($"执行插入数据操作，受影响的返回行数是{Insert()}");
+            Console.WriteLine($"执行修改数据操作，受影响的返回行数是{Update()}");
             Console.ReadKey();
         }
         static void ConnectDB()
@@ -28,6 +30,75 @@ namespace Ado.Net
                 Console.WriteLine("连接失败！");
             }
             sqlConnection.Close();
+        }
+        /// <summary>
+        /// 插入数据操作
+        /// </summary>
+        /// <returns></returns>
+        static int Insert()
+        {
+            string constr = "Server=DESKTOP-KV2R00L\\SQLEXPRESS;DataBase=CourseManageDB;Uid=sa;Pwd=123456";
+            SqlConnection sqlConnection = new SqlConnection(constr);
+            string Insertsql = "insert into Course(CourseName,CourseContent,ClassHour,Credit,CategoryId,TeacherId) ";
+            Insertsql += "values('全栈预科', '第01章【全栈预科】快速入门语法、OOP精华、SQL基础ADO.NET与分层项目', 200, 10, 10, 1003)";
+            SqlCommand sqlCommand = new SqlCommand(Insertsql,sqlConnection);
+            sqlConnection.Open();
+            if (sqlConnection.State==ConnectionState.Open)
+            {
+                Console.WriteLine("数据库连接成功！");
+            }
+            else
+            {
+                Console.WriteLine("数据库连接失败！");
+            }
+            int result= sqlCommand.ExecuteNonQuery();
+            return result;
+        }
+        
+        /// <summary>
+        /// 修改数据操作
+        /// </summary>
+        /// <returns></returns>
+        static int Update()
+        {
+            string constr = "Server=DESKTOP-KV2R00L\\SQLEXPRESS;DataBase=CourseManageDB;Uid=sa;Pwd=123456";
+            SqlConnection sqlConnection = new SqlConnection(constr);
+            string Insertsql = "update Teacher set LoginPwd='123456' where TeacherId=1002";
+            SqlCommand sqlCommand = new SqlCommand(Insertsql, sqlConnection);
+            sqlConnection.Open();
+            if (sqlConnection.State == ConnectionState.Open)
+            {
+                Console.WriteLine("数据库连接成功！");
+            }
+            else
+            {
+                Console.WriteLine("数据库连接失败！");
+            }
+            int result = sqlCommand.ExecuteNonQuery();
+            return result;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        static int Delete()
+        {
+            string constr = "Server=DESKTOP-KV2R00L\\SQLEXPRESS;DataBase=CourseManageDB;Uid=sa;Pwd=123456";
+            SqlConnection sqlConnection = new SqlConnection(constr);
+            string Insertsql = "delete Course where CourseId=1004";
+            SqlCommand sqlCommand = new SqlCommand(Insertsql, sqlConnection);
+            sqlConnection.Open();
+            if (sqlConnection.State == ConnectionState.Open)
+            {
+                Console.WriteLine("数据库连接成功！");
+            }
+            else
+            {
+                Console.WriteLine("数据库连接失败！");
+            }
+            int result = sqlCommand.ExecuteNonQuery();
+            return result;
         }
     }
 }
